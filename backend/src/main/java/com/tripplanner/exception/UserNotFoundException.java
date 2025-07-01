523b1853c4a349537643d10f1bcf55fb2f52fbdd
Ronnasit Tuayton <b6428259@g.sut.ac.th>
2025-07-01 10:14:42 +0700

    Revert "Copilot/fix d63f57f6 c41c 4ea0 840b 881e32494861"

--- backend/src/main/java/com/tripplanner/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tripplanner.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
